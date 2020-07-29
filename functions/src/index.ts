31e57ea3bad65f2d13dc53fa408510f44a5e1c1f
WisaniShilumani <wshilumani@gmail.com>
2020-07-29 17:46:41 +0200

    feature: tweet gbv

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,5 @@
-import * as functions from 'firebase-functions'
-import { quoteMyTweets, tweetHaiku, tweetQuote, addHaiku, addQuote } from './services/twitter'
+import { askGovtAboutGBV } from './services/twitter';
 
 exports.tweets = {
-  quoteMyTweets,
-  tweetHaiku,
-  tweetQuote, 
-  addHaiku: functions.https.onRequest(addHaiku),
-  addQuote: functions.https.onRequest(addQuote),
-}
+  askGovtAboutGBV,
+};
--- functions/src/services/twitter/tweets.ts
+++ functions/src/services/twitter/tweets.ts
@@ -1,167 +1,40 @@
-
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin'
-import twitter, { UPDATE_STATUS, USER_TIMELINE } from './config'
+import * as admin from 'firebase-admin';
+import twitter, { UPDATE_STATUS } from './config';
 
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp(functions.config().firebase);
 
-const cors = require('cors')({
-  origin: true,
-})
+const TIMEZONE = 'Africa/Johannesburg';
 
-interface ResponseData {
-  [key: string]: any;
-}
+const handlesToTweet = ['@CyrilRamaphosa', '@PresidencyZA'];
 
-interface User {
-  id: number;
-  name: string;
-}
+const tweets = [
+  'What laws are you putting in place to #EndGBV in South Africa?',
+  'What are you doing to #EndGBV in South Africa?',
+];
 
-interface ExtendedTweet {
-  full_text: string;
-  display_text_range: number[];
-  entities: any
-}
+const getTwoRandomHandles = (): string => {
+  const shuffledHandles = [...handlesToTweet].sort(() => Math.random() - 0.5);
+  return `${shuffledHandles[0]} ${shuffledHandles[1]}`;
+};
 
-interface Tweet {
-  created_at: string;
-  id: number;
-  text: string;
-  user: User;
-  extended_tweet: ExtendedTweet
-}
+const getRandomTweet = (): string => tweets[Math.round(Math.random() * (tweets.length - 1))];
 
-interface TweetsResponse {
-  statuses: Tweet[]
-}
-
-const USERNAME = '@wisanishilumani'
-const EVERY_HOUR = '0 * * * *'
-
-const btoa = (text: string): string => Buffer.from(text).toString('base64').substr(0, 32)
-
-const updateCallback = (onSuccess: any) => (error: any, response: ResponseData) => {
-  if (error) throw new Error;
-  onSuccess(response)
-}
-
-const promiseCallback = (resolve: any, reject: any) => (error: any, response: ResponseData) => {
-  if (error) return reject(error)
-  resolve(response)
-}
-
-const getUserTweets = async (): Promise<TweetsResponse | any> => {
-  return new Promise((resolve, reject) => {
-    twitter.get(USER_TIMELINE, {
-      screen_name: 'wisanishilumani',
-      include_rts: false,
-      exclude_replies: true
-    }, promiseCallback(resolve, reject))
-  })
-}
-
-const getLastFirebaseObject = (object: any) => {
-  const keys = Object.keys(object)
-  const key = keys[keys.length - 1]
-  return {
-    key,
-    object: object[key]
-  }
-}
-
-
-export const tweetHaiku = functions.pubsub.schedule('5 09 * * *').onRun(async (): Promise<any> => {
-  try {
-    const haikus = await admin.database().ref(`haikus`).once('value')
-    if (!haikus) return null;
-    const { key, object } = getLastFirebaseObject(haikus.val())
-    const onSuccess = async () => {
-      await admin.database().ref(`haikus/${key}`).remove()
-    }
-
-    twitter.post(UPDATE_STATUS, {
-      status: `${object.line1}\n${object.line2}\n${object.line3}\n\n- ${object.author}\n#haiku`
-    }, updateCallback(onSuccess))
-  } catch (error) {
-    console.error('ENCOUNTERED AN ERROR: ', error)
-  }
-
-  return null;
-})
-
-export const tweetQuote = functions.pubsub.schedule('every 6 hours from 11:00 to 00:00').onRun(async (): Promise<any> => {
-  try {
-    const quotes = await admin.database().ref(`quotes`).once('value')
-    if (!quotes) return null;
-    const { key, object } = getLastFirebaseObject(quotes.val())
-    const onSuccess = async () => {
-      await admin.database().ref(`quotes/${key}`).remove()
-    }
-
-    twitter.post(UPDATE_STATUS, {
-      status: `"${object.text}" - \n${object.author}`
-    }, updateCallback(onSuccess))
-  } catch (error) {
-    console.error('ENCOUNTERED AN ERROR: ', error)
-  }
-
-  return null;
-})
-
-export const quoteMyTweets = functions.pubsub.schedule(EVERY_HOUR).onRun(async () => {
-  try {
-    const myTweets: Tweet[] = await getUserTweets()
-    const excludeMentions = (tweet: Tweet) => tweet.text.indexOf('@') === -1
-    const cleanTweets = myTweets.filter(excludeMentions)
-    if (!cleanTweets || !cleanTweets.length) return null;
-    const latestTweet = cleanTweets[0]
-    const tweetKey = btoa(latestTweet.text)
-    const tweetArchive = await admin.database().ref(`tweet_history/${tweetKey}`).once('value')
-    if (!tweetArchive || !tweetArchive.val()) {
-      const onSuccess = async () => {
-        await admin.database().ref('tweet_history').update({ [tweetKey]: latestTweet.text})
+export const askGovtAboutGBV = functions.pubsub
+  .schedule('every 5 minutes from 06:00 to 00:00')
+  .timeZone(TIMEZONE)
+  .onRun(
+    async (): Promise<any> => {
+      try {
+        const randomHandles = getTwoRandomHandles();
+        const tweet = getRandomTweet();
+        await twitter.post(UPDATE_STATUS, {
+          status: `${randomHandles} ${tweet}`,
+        });
+      } catch (error) {
+        console.error('ENCOUNTERED AN ERROR: ', error);
       }
 
-      twitter.post(UPDATE_STATUS, {
-        status: `"${latestTweet.text}" - ${USERNAME}`
-      }, updateCallback(onSuccess))
+      return null;
     }
-  } catch (error) {
-    console.error('ENCOUNTERED AN ERROR: ', error)
-  }
-
-  return null
-});
-
-export const addHaiku = (req: any, res: any) => cors(req, res, async () => {
-  const { line1, line2, line3, author } = req.query
-  try {
-    await admin.database().ref(`haikus`).push({
-      line1,
-      line2,
-      line3,
-      author
-    })
-
-    res.send({ line1, line2, line3, author })
-  } catch (error) {
-    console.error(error)
-    res.send({ error })
-  }
-})
-
-export const addQuote = (req: any, res: any) => cors(req, res, async () => {
-  const { text, author } = req.query
-  try {
-    await admin.database().ref(`quotes`).push({
-      text,
-      author
-    })
-
-    res.send({ text, author })
-  } catch (error) {
-    console.error(error)
-    res.send({ error })
-  }
-})
+  );
